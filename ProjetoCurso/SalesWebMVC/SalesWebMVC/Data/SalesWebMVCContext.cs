82905fe432ee15f381647bc60a82032c73e16654
kaio santos <kaiohvs@gmail.com>
2022-04-28 22:57:22 -0300

    Crud Scaffolding

---
+++ ProjetoCurso/SalesWebMVC/SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -0,0 +1,16 @@
+﻿#nullable disable
+using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Data
+{
+    public class SalesWebMVCContext : DbContext
+    {
+        public SalesWebMVCContext (DbContextOptions<SalesWebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+    }
+}
